ec756ad0c9a6c4593b7b9f807af5cc9273f61440
H-L-down <3107712601@qq.com>
2023-10-25 14:22:22 +0800

    feat: 初始化

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="left-box">
+        <Kline></Kline>
+    </div>
+</template>
+
+<script>
+import Kline from "@/components/Kline.vue";
+
+export default {
+    name: "HomeView",
+    components: {
+        Kline,
+    },
+};
+</script>
+<style scoped>
+.left-box {
+    width: 100%;
+    height: 36%;
+}
+</style>
